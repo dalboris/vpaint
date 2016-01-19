9dc47193ed51bc334432d95ce11544b8d3e7f692
Boris Dalstein <dalboris@gmail.com>
2016-01-19 13:27:30 -0800

    Added new headers to all .h, .cpp and .pro files

--- src/Gui/Background/BackgroundWidget.h
+++ src/Gui/Background/BackgroundWidget.h
@@ -1,3 +1,11 @@
+// Copyright (C) 2012-2016 The VPaint Developers.
+// See the COPYRIGHT file at the top-level directory of this distribution
+// and at https://github.com/dalboris/vpaint/blob/master/COPYRIGHT
+//
+// This file is part of VPaint, a vector graphics editor. It is subject to the
+// license terms and conditions in the LICENSE.MIT file found in the top-level
+// directory of this distribution and at http://opensource.org/licenses/MIT
+
 // This file is part of VPaint, a vector graphics editor.
 //
 // Copyright (C) 2015 Boris Dalstein <dalboris@gmail.com>
--- src/Third/GLEW/GLEW.pro
+++ src/Third/GLEW/GLEW.pro
@@ -1,3 +1,11 @@
+# Copyright (C) 2012-2016 The VPaint Developers.
+# See the COPYRIGHT file at the top-level directory of this distribution
+# and at https://github.com/dalboris/vpaint/blob/master/COPYRIGHT
+#
+# This file is part of VPaint, a vector graphics editor. It is subject to the
+# license terms and conditions in the LICENSE.MIT file found in the top-level
+# directory of this distribution and at http://opensource.org/licenses/MIT
+
 #-------------------------------------------------
 #
 # Project created by QtCreator 2015-08-11T22:59:34
